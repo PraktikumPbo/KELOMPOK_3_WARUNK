5ee41f0904fb1a23833ff81f80a9b99f7b8aac21
gilarwahibul <93990041+gilarwahibul@users.noreply.github.com>
2023-01-16 16:13:31 +0700

    Add files via upload

---
+++ Data/mvcdb.cs
@@ -0,0 +1,14 @@
+﻿using app_testlar.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+namespace app_testlar.Data
+{
+    public class mvcdb : DbContext
+
+    {
+        public mvcdb(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<employe> employes { get; set; }
+    }
+}
